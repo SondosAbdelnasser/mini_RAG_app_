d6a845aa6e215d645bb4005849a4c4a61f2090db
sondos-abdelnasser <132229525+SondosAbdelnasser@users.noreply.github.com>
2025-09-29 01:48:19 +0300

    update main

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from typing import Union
 
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+from routes import base
 
 app = FastAPI()
 
-
-@app.get("/welcome  ")
-def welcome():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(base.base_router)
